504dbbfca900f40dda906604bc93db8e53a7306b
Nemkev <ostegoelectric@gmail.com>
2020-07-24 14:36:11 +0300

    feat: add entity for reports

---
+++ src/entities/reports.ts
@@ -0,0 +1,49 @@
+import {
+  Column,
+  Entity,
+  PrimaryGeneratedColumn,
+  JoinColumn,
+  ManyToOne,
+} from "typeorm";
+
+import { User } from "@entities/user";
+
+@Entity("reports")
+export class Reports {
+  @PrimaryGeneratedColumn("uuid")
+  public id: string;
+
+  @Column({
+    type: "varchar",
+    length: 1000,
+    nullable: true,
+  })
+  public description: string;
+
+  @Column({
+    type: "timestamp",
+    nullable: true,
+  })
+  public date: Date;
+
+  @Column({
+    type: "int",
+    nullable: true,
+  })
+  public time: number;
+
+  @Column({
+    type: "uuid",
+    nullable: true,
+  })
+  public user_id: string;
+
+  @ManyToOne("User", "reports", {
+    onDelete: "CASCADE",
+  })
+  @JoinColumn({
+    name: "user_id",
+    referencedColumnName: "id",
+  })
+  public user: User;
+}
